bcd05335eb57fabe0a286d8a0dd15a38fb987bda
cthuff <cthuff03@gmail.com>
2016-11-08 08:26:04 -0800

    UndoManager classes added. No implementation

---
+++ violet-0.16b/com/horstmann/violet/UndoManager.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+/**
+ * This class manage a stack of UndoableEdits that can later be undone and
+ * redone in the graph
+ * 
+ * @author craig
+ *
+ */
+public class UndoManager
+{
+    public UndoManager()
+    {
+         
+    }
+    
+    public void add(UndoableEdit )
+    {
+        
+    }
+    
+    public void undo()
+    {
+        
+    }
+    
+    public void redo()
+    {
+        
+    }
+    
+    private Stack<UndoableEdit> edits;
+}
---
+++ violet-0.16b/com/horstmann/violet/UndoableEdit.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @author craig
+ *
+ */
+public class UndoableEdit
+{
+
+}
