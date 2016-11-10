f5f40128b85420e507a374ee38f060b7ab15db62
HengYiShi <qweasdzxc962453@gmail.com>
2016-11-09 21:41:19 -0800

    11/9 finished undo redo function it will now work on node. will finish function later so it can also works on edges

--- violet-0.16b/com/horstmann/violet/PointNode.java
+++ violet-0.16b/com/horstmann/violet/PointNode.java
@@ -71,4 +71,13 @@ public class PointNode extends AbstractNode
    }
 
    private Point2D point;
+
+   @Override
+   public double[] location()
+   {
+      double[] a = new double[2];
+      a[0] = point.getX();
+      a[1] = point.getY();
+      return a;
+   }
 }
