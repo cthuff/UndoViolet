31fd209b61810bc09fc1422949d7fb5c0f0e8f35
cthuff <cthuff03@gmail.com>
2016-11-15 20:00:38 -0800

    Checkstyle xml file added

--- violet-0.16b/com/horstmann/violet/UndoManager.java
+++ violet-0.16b/com/horstmann/violet/UndoManager.java
@@ -11,16 +11,28 @@ import javax.swing.undo.UndoableEdit;
  */
 public class UndoManager
 {
+    /**
+     * Constructs an UndoableEdit Stack
+     * 
+     * @param editable Stack to be initialized
+     */
     public UndoManager(Stack<UndoableEdit> editable)
     {
         this.edits = editable;
     }
 
+    /**
+     * Adds the newest edit to the stack
+     * @param e the edit to be added to the Stack
+     */
     public void add(UndoableEdit e)
     {
         edits.add(e);
     }
 
+    /**
+     * When the undo button is clicked 
+     */
     public void undo()
     {
 
